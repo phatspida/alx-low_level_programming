db052953243d4065cd1dc2051396c1ef66d6fb6c
Ahmed Yakubu <ahmedmomodu97@gmail.com>
2022-10-14 14:31:22 -0700

    Changes made

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - func
+ * Prints single digit base10 numbers starting from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 10; num < 10; num++)
+		printf("%d", num);
+
+	printf("\n");
+
+
+	return (0);
+
+}
