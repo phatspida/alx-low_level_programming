fd7373302840082023f301eda2877643607929e0
Ahmed Yakubu <ahmedmomodu97@gmail.com>
2022-10-14 15:20:23 -0700

    j

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,9 @@ int main(void)
 	int num;
 	char letter;
 	for (num = 0; num < 10; num++)
-		putchar((num % 10) + '0');
+		putchar(num);
 	for (letter = 'a'; letter <= 'f'; letter++)
 		putchar(letter);
-		putchar('\n');
+	putchar('\n');
 	return (0);
 }
