3835408a9a1f720be0367618e3f4c55b32d82e6c
Ahmed Yakubu <ahmedmomodu97@gmail.com>
2022-10-14 14:07:25 -0700

    editted

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - func
+ * Prints lower characters.
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+		printf("\n");
+
+	return (0);
+
+}
