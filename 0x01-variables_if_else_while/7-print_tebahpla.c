f538c09bb4ea1704ff7fabe1a5875d36e671fa43
Ahmed Yakubu <ahmedmomodu97@gmail.com>
2022-10-14 14:45:06 -0700

    Updated

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - func
+ * Prints lower reverse alphabets
+ * * Return: 0
+ */
+int main(void)
+{
+	char letter;
+	
+	for (letter = 'z'; letter >= 'a'; letter --)
+		putchar(letter);
+
+	putchar('\n');
+
+
+	return (0);
+
+}
