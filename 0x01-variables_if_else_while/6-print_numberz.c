2b71774aa0fda2c769bb6cb5cbb753702b9fabee
Ahmed Yakubu <ahmedmomodu97@gmail.com>
2022-10-14 14:37:11 -0700

    Changes made

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - func
+ * Prints single digit base10 numbers starting from 0 using putchar only.
+ * Return: 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar("\n");
+
+
+	return (0);
+
+}
