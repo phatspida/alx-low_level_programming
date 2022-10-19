e93db0ec6563f520f5608d22cc41aa9859ced8df
Ahmed Yakubu <ahmedmomodu97@gmail.com>
2022-10-19 15:29:50 -0700

    prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,14 +10,13 @@
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char c[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
+	int i;
+
+	for (i = 0; i <= 8; i++)
+	{
+		_putchar(c[i]);
+	}
 	_putchar('\n');
 
 	return (0);
