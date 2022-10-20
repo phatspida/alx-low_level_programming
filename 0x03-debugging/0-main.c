141426035131e72ae5aee8188f58baffc1307e23
Ahmed Yakubu <ahmedmomodu97@gmail.com>
2022-10-20 14:28:34 -0700

    Finding the largest of  three numbers

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function positive_or_negative() gives the correct output
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
---
+++ 0x03-debugging/main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+* main - tests function that prints if integer is positive or negative.
+*
+* description: 'Checks the positive_or_negative function,
+* as could be used.'
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
