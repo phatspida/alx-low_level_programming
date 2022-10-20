ae4b85f5de5021aa1cb55e9a569c4738b7f58574
Ahmed Yakubu <ahmedmomodu97@gmail.com>
2022-10-20 14:57:32 -0700

    Testing value

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - funct
+ * Gets a random number and test to
+ * see if it is either greter than,
+ * less than or equal to zero
+ * Return: 0
+ */
+
+int positive_or_negative(int value)
+{
+	int n;
+	
+	n = value;
+
+	if (n > 0)
+		printf("%i is positive\n", n);
+	else if (n < 0)
+		printf("%i is negative\n", n);
+	else
+		printf("%i is zero\n", n);
+	return (0);
+}
