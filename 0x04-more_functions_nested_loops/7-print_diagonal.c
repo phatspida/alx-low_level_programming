f68efb1d7f79164e5b129ea6d2ff2efc1cfa6f78
Ahmed Yakubu <ahmedmomodu97@gmail.com>
2022-10-20 18:55:36 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Print diagonal
+ * @n: amount to be printed
+ *
+ * Return: Always return 0
+ */
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n > 0)
+	{
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < n; b++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
