0f71efc918b3ccb376ec951e9afa7c5c6b264948
Ahmed Yakubu <ahmedmomodu97@gmail.com>
2022-10-20 17:33:56 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,6 +3,8 @@
 /**
  * tests for upper case character.
  * @c: The input character.
+ *
+ * Description: function compare input to test.
  * Return: 1 if c is uppercase and 0 otherwise.
  */
 int _isupper(int c)
@@ -10,6 +12,10 @@ int _isupper(int c)
 	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
+	}
+	else if (c <= 'z')
+	{
+		return (0);
 	}
 	else
 	{
